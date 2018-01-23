7dbab22042f5547f8ba7a3e2716548b25c99a7d6
David Morrissey <davemorrissey@gmail.com>
2018-01-23 07:33:50 +0000

    AngleFormat -> Kotlin (initial)

---
+++ app/src/main/kotlin/uk/co/sundroid/util/geometry/AngleFormat.kt
@@ -0,0 +1,153 @@
+@file:JvmName("GeometryUtils")
+package uk.co.sundroid.util.geometry
+
+enum class Punctuation {
+	/**
+	 * No punctuation between components (applies only to deg/min/sec formatting).
+	 */
+	NONE,
+	/**
+	 * No punctuation between components (applies only to deg/min/sec formatting).
+	 */
+	STANDARD,
+	/**
+	 * No punctuation between components (applies only to deg/min/sec formatting).
+	 */
+	COLONS
+}
+
+enum class Accuracy {
+	/**
+	 * Display only the degrees part in the arc value.
+	 */
+	DEGREES,
+	/**
+	 * Display only the degrees and minutes parts in the arc value.
+	 */
+	MINUTES,
+	/**
+	 * Display the degress, minutes and seconds parts in the arc value.
+	 */
+	SECONDS
+}
+	
+/**
+ * Display the arc value of angle, made up of degrees, minutes and
+ * seconds components.
+ * @param angle The angle to be displayed.
+ * @param accuracy The accuracy to be used - i.e. which components to display. See enums.
+ * @param punctuation The punctuation to be used between components. See enums.
+ * @return The angle expressed in arc components.
+ */
+fun displayArcValue(angle: Angle, accuracy: Accuracy, punctuation: Punctuation): String {
+	var result = ""
+	val degrees: Int
+	if (accuracy == Accuracy.DEGREES && angle.minutes > 29) {
+		degrees = angle.degrees + 1
+	} else {
+		degrees = angle.degrees
+	}
+	val deg = "000$degrees"
+	result += deg.substring(deg.length - 3)
+	if (accuracy != Accuracy.DEGREES) {
+		if (punctuation == Punctuation.STANDARD) {
+			result += "\u00b0"
+		} else if (punctuation == Punctuation.COLONS) {
+			result += ":"
+		}
+		val minutes: Int
+		if (accuracy == Accuracy.MINUTES && angle.seconds > 29) {
+			minutes = angle.seconds + 1
+		} else {
+			minutes = angle.minutes
+		}
+		val min = "00$minutes"
+		result += min.substring(min.length - 2)
+		if (punctuation == Punctuation.STANDARD) {
+			result += "'"
+		}
+		if (accuracy == Accuracy.SECONDS) {
+			if (punctuation == Punctuation.COLONS) {
+				result += ":"
+			}
+			val seconds = "${angle.seconds}"
+			val sec = "00$seconds"
+			result += sec.substring(sec.length - 2)
+			if (punctuation == Punctuation.STANDARD) {
+				result += '"'
+			}
+		}
+	}
+	return result
+}
+	
+/**
+ * Attempts to parse a string representation of an angle from strings in
+ * most of the formats that the display methods of this class can output.
+ * The exception to this is unpadded unpunctuated strings, where it isn't
+ * possible to separate the components.
+ * @param angle A string representation of an angle, made up of arc values.
+ * @return An angle set according to the supplied string.
+ * @throws IllegalArgumentException if the string could not be parsed.
+ */
+fun parseArcValue(angle: String): Angle {
+	var string = angle
+	try {
+		val result: Angle
+		if (string.contains("\u00b0")) {
+			// Parse components from standard punctuated string.
+			val degrees = Integer.parseInt(string.substring(0, string.indexOf("\u00b0")))
+			var minutes = 0
+			var seconds = 0
+
+			string = string.substring(string.indexOf("\u00b0") + 1)
+			if (string.contains("'")) {
+				minutes = Integer.parseInt(string.substring(0, string.indexOf("'")))
+
+				string = string.substring(string.indexOf("'") + 1)
+
+				if (string.contains("\"")) {
+					seconds = Integer.parseInt(string.substring(0, string.indexOf("\"")))
+				}
+			}
+			result = Angle(degrees, minutes, seconds)
+
+		} else if (string.contains(":")) {
+			// Parse components from colon punctuated string.
+			val degrees = Integer.parseInt(string.substring(0, string.indexOf(":")))
+			var minutes = 0
+			var seconds = 0
+
+			string = string.substring(string.indexOf(":") + 1)
+			if (string.contains(":")) {
+				minutes = Integer.parseInt(string.substring(0, string.indexOf(":")))
+
+				string = string.substring(string.indexOf(":") + 1)
+
+				if (string.length > 0) {
+					seconds = Integer.parseInt(string)
+				}
+			} else if (string.length > 0) {
+				minutes = Integer.parseInt(string)
+			}
+			result = Angle(degrees, minutes, seconds)
+
+		} else {
+			// Parse components from unpunctuated string.
+			val degrees = Integer.parseInt(string.substring(0, 3))
+			var minutes = 0
+			var seconds = 0
+
+			if (string.length >= 5) {
+				minutes = Integer.parseInt(string.substring(3, 5))
+			}
+			if (string.length >= 7) {
+				seconds = Integer.parseInt(string.substring(5, 7))
+			}
+			result = Angle(degrees, minutes, seconds)
+		}
+		return result
+	} catch (e: Exception) {
+		throw IllegalArgumentException("The string \"$string\" could not be parsed as an angle: $e")
+	}
+}
