3694543013e112e6cfe594bac30503b7b1d563fd
David Morrissey <davemorrissey@gmail.com>
2018-01-25 07:33:49 +0000

    Tabs -> spaces

--- app/src/main/kotlin/uk/co/sundroid/util/location/LatitudeLongitude.kt
+++ app/src/main/kotlin/uk/co/sundroid/util/location/LatitudeLongitude.kt
@@ -11,105 +11,105 @@ import java.text.ParseException
 class LatitudeLongitude : Serializable {
 
     /**
-	 * The latitude angle.
-	 */
-	var latitude = Latitude(0.0)
-		get() = Latitude(latitude.doubleValue)
-		private set
-	
-	/**
-	 * The longitude angle.
-	 */
-	var longitude = Longitude(0.0)
-		get() = Longitude(longitude.doubleValue)
-		private set
-	
-	/**
-	 * Create a point from prepared lat and long values.
-	 * @param latitude The latitude to set.
-	 * @param longitude The longitude to set.
-	 * @throws IllegalArgumentException if either the lat or long is null.
-	 */
-	constructor(latitude: Latitude, longitude: Longitude) {
-		this.latitude = latitude
-		this.longitude = longitude
-	}
-	
-	/**
-	 * Create a point from two double values.
-	 * @param latitude The latitude to set.
-	 * @param longitude The longitude to set.
-	 * @throws IllegalArgumentException if either the lat or long is out of range.
-	 */
-	constructor(latitude: Double, longitude: Double) {
-		this.latitude = Latitude(latitude)
-		this.longitude = Longitude(longitude)
-	}
-	
-	/**
-	 * Create a point from two string values in abbreviated format.
-	 * @param latitude A valid abbreviated latitude string.
-	 * @param longitude A valid abbreviated longitude string.
-	 * @throws ParseException if either the lat or long could not be parsed.
-	 */
-	constructor(latitude: String, longitude: String) {
-		this.latitude = Latitude(latitude)
-		this.longitude = Longitude(longitude)
-	}
-	
-	/**
-	 * Create a point from two string values in abbreviated format separated by
-	 * a space e.g. ddmmssN dddmmssE.
-	 * @param latitudeLongitude Abbreviated lat and long values separated by a single space.
-	 * @throws ParseException if the string could not be parsed.
-	 */
-	constructor(latitudeLongitude: String) {
-		if (latitudeLongitude.contains(" ")) {
-			this.latitude = Latitude(latitudeLongitude.substring(0, latitudeLongitude.indexOf(" ")))
-			this.longitude = Longitude(latitudeLongitude.substring(latitudeLongitude.indexOf(" ") + 1))
-		} else {
-			throw ParseException("Could not parse lat/long coordinate from abbreviated string \"$latitudeLongitude\"", 0)
-		}
-	}
-	
-	/**
-	 * Gets the co-ordinate in abbreviated format e.g. ddmmssN dddmmssE
-	 */
-	fun getAbbreviatedValue(): String {
-		return latitude.getAbbreviatedValue() + " " + longitude.getAbbreviatedValue()
-	}
-	
-	/**
-	 * Gets the co-ordinate in standard punctuated format with specified accuracy.
-	 */
-	fun getPunctuatedValue(accuracy: Accuracy): String {
-		return latitude.getPunctuatedValue(accuracy) + " " + longitude.getPunctuatedValue(accuracy)
-	}
-	
-	/**
-	 * String display returns abbreviated value.
-	 */
-	override fun toString(): String {
-		return getAbbreviatedValue()
-	}
-	
-	/**
-	 * Compare two locations for equality. They are considered the same if
-	 * the displayed abbreviated values (which include degrees, minutes and
-	 * seconds) are the same. Because of this, locations with very slightly
-	 * different double values may be considered equal but the error equates
-	 * to a maximum of about 15 metres.
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	override fun equals(other: Any?): Boolean {
-		if (other !is Latitude) {
-			return false
-		}
-		return other.toString() == toString()
-	}
+     * The latitude angle.
+     */
+    var latitude = Latitude(0.0)
+        get() = Latitude(latitude.doubleValue)
+        private set
+    
+    /**
+     * The longitude angle.
+     */
+    var longitude = Longitude(0.0)
+        get() = Longitude(longitude.doubleValue)
+        private set
+    
+    /**
+     * Create a point from prepared lat and long values.
+     * @param latitude The latitude to set.
+     * @param longitude The longitude to set.
+     * @throws IllegalArgumentException if either the lat or long is null.
+     */
+    constructor(latitude: Latitude, longitude: Longitude) {
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+    
+    /**
+     * Create a point from two double values.
+     * @param latitude The latitude to set.
+     * @param longitude The longitude to set.
+     * @throws IllegalArgumentException if either the lat or long is out of range.
+     */
+    constructor(latitude: Double, longitude: Double) {
+        this.latitude = Latitude(latitude)
+        this.longitude = Longitude(longitude)
+    }
+    
+    /**
+     * Create a point from two string values in abbreviated format.
+     * @param latitude A valid abbreviated latitude string.
+     * @param longitude A valid abbreviated longitude string.
+     * @throws ParseException if either the lat or long could not be parsed.
+     */
+    constructor(latitude: String, longitude: String) {
+        this.latitude = Latitude(latitude)
+        this.longitude = Longitude(longitude)
+    }
+    
+    /**
+     * Create a point from two string values in abbreviated format separated by
+     * a space e.g. ddmmssN dddmmssE.
+     * @param latitudeLongitude Abbreviated lat and long values separated by a single space.
+     * @throws ParseException if the string could not be parsed.
+     */
+    constructor(latitudeLongitude: String) {
+        if (latitudeLongitude.contains(" ")) {
+            this.latitude = Latitude(latitudeLongitude.substring(0, latitudeLongitude.indexOf(" ")))
+            this.longitude = Longitude(latitudeLongitude.substring(latitudeLongitude.indexOf(" ") + 1))
+        } else {
+            throw ParseException("Could not parse lat/long coordinate from abbreviated string \"$latitudeLongitude\"", 0)
+        }
+    }
+    
+    /**
+     * Gets the co-ordinate in abbreviated format e.g. ddmmssN dddmmssE
+     */
+    fun getAbbreviatedValue(): String {
+        return latitude.getAbbreviatedValue() + " " + longitude.getAbbreviatedValue()
+    }
+    
+    /**
+     * Gets the co-ordinate in standard punctuated format with specified accuracy.
+     */
+    fun getPunctuatedValue(accuracy: Accuracy): String {
+        return latitude.getPunctuatedValue(accuracy) + " " + longitude.getPunctuatedValue(accuracy)
+    }
+    
+    /**
+     * String display returns abbreviated value.
+     */
+    override fun toString(): String {
+        return getAbbreviatedValue()
+    }
+    
+    /**
+     * Compare two locations for equality. They are considered the same if
+     * the displayed abbreviated values (which include degrees, minutes and
+     * seconds) are the same. Because of this, locations with very slightly
+     * different double values may be considered equal but the error equates
+     * to a maximum of about 15 metres.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    override fun equals(other: Any?): Boolean {
+        if (other !is Latitude) {
+            return false
+        }
+        return other.toString() == toString()
+    }
 
-	companion object {
-		private const val serialVersionUID = -9133219631765541150L
-	}
+    companion object {
+        private const val serialVersionUID = -9133219631765541150L
+    }
 
 }
