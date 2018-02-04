07b99eeae6b399d07ee585fe4ee668844fb1a459
David Morrissey <davemorrissey@gmail.com>
2018-02-04 08:21:27 +0000

    Unused constructors removed

--- app/src/main/kotlin/uk/co/sundroid/util/time/Clock.kt
+++ app/src/main/kotlin/uk/co/sundroid/util/time/Clock.kt
@@ -2,14 +2,29 @@ package uk.co.sundroid.util.time
 
 import uk.co.sundroid.util.zeroPad
 
-class Clock {
+class Clock(hours: Double, private val showSeconds: Boolean) {
 
-    val h: Int
-    val m: Int
-    val s: Int
-    private val showSeconds: Boolean
+    private val h: Int
+    private val m: Int
+    private val s: Int
 
-    constructor(hours: Double, showSeconds: Boolean) {
+    fun toHMS(): String {
+        return if (showSeconds) {
+            h.toString() + "h " + zeroPad(m, 2) + "m " + zeroPad(s, 2) + "s"
+        } else {
+            h.toString() + "h " + zeroPad(m, 2) + "m"
+        }
+    }
+
+    fun toClock(): String {
+        return if (showSeconds) {
+            h.toString() + ":" + zeroPad(m, 2) + ":" + zeroPad(s, 2)
+        } else {
+            h.toString() + ":" + zeroPad(m, 2)
+        }
+    }
+
+    init {
         var h = Math.floor(hours).toInt()
         var m = Math.floor((hours - h) * 60.0).toInt()
         var s = 0
@@ -27,37 +42,6 @@ class Clock {
         this.h = h
         this.m = m
         this.s = s
-        this.showSeconds = showSeconds
-    }
-
-    constructor(h: Int, m: Int) {
-        this.h = h
-        this.m = m
-        this.s = 0
-        this.showSeconds = false
-    }
-
-    constructor(h: Int, m: Int, s: Int) {
-        this.h = h
-        this.m = m
-        this.s = s
-        this.showSeconds = true
-    }
-
-    fun toHMS(): String {
-        return if (showSeconds) {
-            h.toString() + "h " + zeroPad(m, 2) + "m " + zeroPad(s, 2) + "s"
-        } else {
-            h.toString() + "h " + zeroPad(m, 2) + "m"
-        }
-    }
-
-    fun toClock(): String {
-        return if (showSeconds) {
-            h.toString() + ":" + zeroPad(m, 2) + ":" + zeroPad(s, 2)
-        } else {
-            h.toString() + ":" + zeroPad(m, 2)
-        }
     }
 
 }
