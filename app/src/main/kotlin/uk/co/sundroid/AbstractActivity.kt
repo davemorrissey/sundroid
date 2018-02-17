921e67428ee37cc8485e2a80bc0a90d9bfa933b0
David Morrissey <davemorrissey@gmail.com>
2018-02-17 11:06:48 +0000

    AbstractActivity/Fragment -> Kotlin

---
+++ app/src/main/kotlin/uk/co/sundroid/AbstractActivity.kt
@@ -0,0 +1,146 @@
+package uk.co.sundroid
+
+import android.app.ActionBar
+import android.app.Activity
+import android.os.Bundle
+import android.view.*
+import android.view.View.OnClickListener
+import android.widget.TextView
+
+import uk.co.sundroid.util.theme.*
+
+import uk.co.sundroid.NavItem.NavItemLocation.*
+
+/**
+ * Provides some helper functions for activities.
+ */
+abstract class AbstractActivity : Activity(), OnClickListener {
+
+    private var navItems: List<NavItem>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onActivityCreateSetTheme(this)
+        window.requestFeature(Window.FEATURE_ACTION_BAR)
+        window.setBackgroundDrawableResource(getAppBg())
+        val actionBar = actionBar
+        actionBar?.setBackgroundDrawable(resources.getDrawable(getActionBarBg()))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.clear()
+        if (navItems != null) {
+            for (navItem in navItems!!) {
+                val menuItem = menu.add(Menu.NONE, navItem.action, Menu.NONE, navItem.title)
+                if (navItem.icon > 0) {
+                    menuItem.setIcon(navItem.icon)
+                }
+                if (navItem.location === HEADER) {
+                    menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+                } else if (navItem.location === HEADER_IF_ROOM) {
+                    menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+                } else {
+                    menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (navItems != null) {
+            for (navItem in navItems!!) {
+                if (navItem.action == item.itemId) {
+                    onNavItemSelected(navItem.action)
+                    return true
+                }
+            }
+        }
+        onNavBackSelected()
+        return true
+    }
+
+    override fun onClick(view: View) {
+
+    }
+
+    protected open fun onNavItemSelected(itemPosition: Int) {
+
+    }
+
+    protected open fun onNavBackSelected() {
+
+    }
+
+    protected fun setNavItems(navItems: List<NavItem>) {
+        this.navItems = navItems
+        invalidateOptionsMenu()
+    }
+
+    @JvmOverloads
+    protected fun setActionBarTitle(title: String, subtitle: String? = null) {
+        val actionBar = actionBar
+        if (actionBar != null) {
+            actionBar.title = title
+            actionBar.subtitle = subtitle
+        }
+    }
+
+    protected fun setDisplayHomeAsUpEnabled() {
+        val actionBar = actionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    protected fun text(id: Int, text: String) {
+        val view = findViewById<TextView>(id)
+        view.text = text
+    }
+
+    protected fun show(id: Int, text: CharSequence) {
+        val view = findViewById<TextView>(id)
+        view.visibility = View.VISIBLE
+        view.text = text
+    }
+
+    protected fun show(vararg ids: Int) {
+        for (id in ids) {
+            findViewById<View>(id).visibility = View.VISIBLE
+        }
+    }
+
+    protected fun remove(vararg ids: Int) {
+        for (id in ids) {
+            findViewById<View>(id).visibility = View.GONE
+        }
+    }
+
+    protected fun hide(vararg ids: Int) {
+        for (id in ids) {
+            findViewById<View>(id).visibility = View.INVISIBLE
+        }
+    }
+
+    protected fun textInView(view: View, id: Int, text: String) {
+        val child = view.findViewById<TextView>(id)
+        child.text = text
+    }
+
+    protected fun showInView(view: View, id: Int, text: CharSequence) {
+        val child = view.findViewById<TextView>(id)
+        child.visibility = View.VISIBLE
+        child.text = text
+    }
+
+    protected fun showInView(view: View, vararg ids: Int) {
+        for (id in ids) {
+            view.findViewById<View>(id).visibility = View.VISIBLE
+        }
+    }
+
+    protected fun removeInView(view: View, vararg ids: Int) {
+        for (id in ids) {
+            view.findViewById<View>(id).visibility = View.GONE
+        }
+    }
+
+}
---
+++ app/src/main/kotlin/uk/co/sundroid/AbstractFragment.kt
@@ -0,0 +1,65 @@
+package uk.co.sundroid
+
+import android.app.Activity
+import android.app.Fragment
+import android.content.Context
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import uk.co.sundroid.R.id
+
+/**
+ * Provides some helper functions for fragments.
+ */
+abstract class AbstractFragment : Fragment() {
+
+    protected val applicationContext: Context?
+        get() {
+            val activity = activity
+            return activity?.applicationContext
+        }
+
+    protected fun textInView(view: View, id: Int, text: CharSequence) {
+        val child = view.findViewById<TextView>(id)
+        child.text = text
+    }
+
+    protected fun showInView(view: View, id: Int, text: CharSequence) {
+        val child = view.findViewById<TextView>(id)
+        child.visibility = View.VISIBLE
+        child.text = text
+    }
+
+    protected fun showInView(view: View, vararg ids: Int) {
+        for (id in ids) {
+            view.findViewById<View>(id).visibility = View.VISIBLE
+        }
+    }
+
+    protected fun removeInView(view: View, vararg ids: Int) {
+        for (id in ids) {
+            view.findViewById<View>(id).visibility = View.GONE
+        }
+    }
+
+    protected fun hideInView(view: View, vararg ids: Int) {
+        for (id in ids) {
+            view.findViewById<View>(id).visibility = View.INVISIBLE
+        }
+    }
+
+    protected fun imageInView(view: View, id: Int, drawable: Int) {
+        (view.findViewById<View>(id) as ImageView).setImageResource(drawable)
+    }
+
+    @Throws(IllegalArgumentException::class)
+    protected fun view(name: String): Int {
+        try {
+            return id::class.java.getField(name).getInt(null)
+        } catch (e: Exception) {
+            throw IllegalArgumentException("No view with id $name exists")
+        }
+
+    }
+
+}
