c0307739bd508cc3f327e426a07397efbbb3fe44
David Morrissey <davemorrissey@gmail.com>
2018-02-24 09:14:28 +0000

    Refactored for null safety, and to not retain instance on rotate

--- app/src/main/kotlin/uk/co/sundroid/activity/data/fragments/dialogs/date/DatePickerFragment.kt
+++ app/src/main/kotlin/uk/co/sundroid/activity/data/fragments/dialogs/date/DatePickerFragment.kt
@@ -3,7 +3,6 @@ package uk.co.sundroid.activity.data.fragments.dialogs.date
 import android.app.AlertDialog
 import android.app.Dialog
 import android.app.DialogFragment
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
@@ -12,15 +11,12 @@ import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import uk.co.sundroid.R
 import uk.co.sundroid.R.layout
-
-import java.util.Calendar
-import java.util.TimeZone
-
+import java.util.*
 import java.util.Calendar.*
 
-class DatePickerFragment : DialogFragment(), DialogInterface.OnClickListener, OnDateChangedListener, OnSeekBarChangeListener {
+class DatePickerFragment : DialogFragment(), OnDateChangedListener, OnSeekBarChangeListener {
 
-    private var calendar: Calendar? = null
+    private var calendar: Calendar = Calendar.getInstance()
     private var datePicker: DatePicker? = null
     private var dateSeeker: SeekBar? = null
 
@@ -31,53 +27,48 @@ class DatePickerFragment : DialogFragment(), DialogInterface.OnClickListener, On
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val ymd = arguments.getIntArray("ymd")
-        val tz = arguments.getString("tz")
-        calendar = Calendar.getInstance()
-        if (ymd != null && tz != null) {
-            calendar!!.timeZone = TimeZone.getTimeZone(tz)
-            calendar!!.set(ymd[0], ymd[1], ymd[2])
-        }
+        restore(arguments)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Companion.save(calendar, outState)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        retainInstance = true // TODO May not be required
-        val builder = AlertDialog.Builder(activity)
+        restore(savedInstanceState)
 
         val view = View.inflate(activity, layout.dialog_datepicker, null)
+
         datePicker = view.findViewById(R.id.datePicker)
-        datePicker!!.init(calendar!!.get(YEAR), calendar!!.get(MONTH), calendar!!.get(DAY_OF_MONTH), this)
-        builder.setView(view)
-        builder.setTitle("Set date")
-
+        datePicker?.apply {
+            init(calendar.get(YEAR), calendar.get(MONTH), calendar.get(DAY_OF_MONTH), this@DatePickerFragment)
+        }
         dateSeeker = view.findViewById(R.id.dateSeeker)
-        dateSeeker!!.setOnSeekBarChangeListener(this)
-        dateSeeker!!.max = 365
-        dateSeeker!!.progress = calendar!!.get(DAY_OF_YEAR)
+        dateSeeker?.apply {
+            setOnSeekBarChangeListener(this@DatePickerFragment)
+            max = 365
+            progress = calendar.get(DAY_OF_YEAR)
+        }
 
-        builder.setPositiveButton("Set", this)
-        builder.setNeutralButton("Today", this)
-        builder.setNegativeButton("Cancel", this)
-        return builder.create()
+        return AlertDialog.Builder(activity).apply {
+            setView(view)
+            setTitle("Set date")
+            setPositiveButton("Set", { _, _ -> set() })
+            setNeutralButton("Today", { _, _ -> calendar.timeInMillis = System.currentTimeMillis(); set() })
+            setNegativeButton("Cancel", { _, _  -> })
+        }.create()
     }
-
-
 
     override fun onDateChanged(datePicker: DatePicker, year: Int, month: Int, day: Int) {
-        if (dateSeeker != null && calendar != null) {
-            val cal = Calendar.getInstance(calendar!!.timeZone)
-            cal.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
-            dateSeeker!!.progress = cal.get(DAY_OF_YEAR)
-        }
+        calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+        dateSeeker?.progress = calendar.get(DAY_OF_YEAR)
     }
 
-    override fun onProgressChanged(seekBar: SeekBar, dayOfYear: Int, fromUser: Boolean) {
-        var dayOfYear = dayOfYear
-        if (fromUser && datePicker != null && calendar != null) {
-            dayOfYear = Math.max(1, dayOfYear)
-            val cal = Calendar.getInstance(calendar!!.timeZone)
-            cal.set(Calendar.DAY_OF_YEAR, dayOfYear)
-            datePicker!!.updateDate(datePicker!!.year, cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
+    override fun onProgressChanged(dateSeeker: SeekBar, dayOfYear: Int, fromUser: Boolean) {
+        if (fromUser) {
+            calendar.set(Calendar.DAY_OF_YEAR, Math.max(1, dayOfYear))
+            datePicker?.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
         }
     }
 
@@ -85,27 +76,36 @@ class DatePickerFragment : DialogFragment(), DialogInterface.OnClickListener, On
 
     override fun onStopTrackingTouch(seekBar: SeekBar) {}
 
-    override fun onClick(dialogInterface: DialogInterface, button: Int) {
+    fun set() {
         val target = targetFragment
-        if (target != null && target is OnDateSelectedListener) {
-            if (button == DialogInterface.BUTTON_NEUTRAL) {
-                val today = Calendar.getInstance()
-                (target as OnDateSelectedListener).onDateSet(today.get(YEAR), today.get(MONTH), today.get(DAY_OF_MONTH))
-            } else if (button == DialogInterface.BUTTON_POSITIVE) {
-                (target as OnDateSelectedListener).onDateSet(datePicker!!.year, datePicker!!.month, datePicker!!.dayOfMonth)
+        if (target is OnDateSelectedListener) {
+            (target as OnDateSelectedListener).onDateSet(calendar.get(YEAR), calendar.get(MONTH), calendar.get(DAY_OF_MONTH))
+        }
+    }
+
+    private fun restore(bundle: Bundle?) {
+        if (bundle != null) {
+            val ymd = bundle.getIntArray("ymd")
+            val tz = bundle.getString("tz")
+            if (ymd != null && tz != null) {
+                calendar.timeZone = TimeZone.getTimeZone(tz)
+                calendar.set(ymd[0], ymd[1], ymd[2])
             }
         }
-        dismiss()
     }
 
     companion object {
         fun newInstance(calendar: Calendar): DatePickerFragment {
-            val fragment = DatePickerFragment()
-            val args = Bundle()
-            args.putIntArray("ymd", intArrayOf(calendar.get(YEAR), calendar.get(MONTH), calendar.get(DAY_OF_MONTH)))
-            args.putString("tz", calendar.timeZone.id)
-            fragment.arguments = args
-            return fragment
+            return DatePickerFragment().apply {
+                arguments = save(calendar)
+            }
+        }
+
+        private fun save(calendar: Calendar, bundle: Bundle? = Bundle()): Bundle? {
+            return bundle?.apply {
+                putIntArray("ymd", intArrayOf(calendar.get(YEAR), calendar.get(MONTH), calendar.get(DAY_OF_MONTH)))
+                putString("tz", calendar.timeZone.id)
+            }
         }
     }
 
